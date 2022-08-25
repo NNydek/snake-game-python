1079a316e47470ed9b74cb8dc9ce526881d0ac25
Nydek <nnydek@gmail.com>
2022-08-25 15:25:53 +0200

    rename food file, add new function
    Change food.py to spawn.py
    Create draw_food function

--- Food/spawn.py
+++ Food/spawn.py
@@ -1,12 +1,18 @@
+import pygame
 import random
-from Game.classes import Dimension
+from Game.classes import Color, Dimension
+from Game import game_window
 
 width = Dimension.WIDTH.value
 height = Dimension.HEIGHT.value
 pixel = Dimension.PIXEL.value
 
-def generate():
+def generate_food():
     new_x = round(random.randrange(0, width - pixel) / pixel) * pixel
     new_y = round(random.randrange(0, height - pixel) / pixel) * pixel
     return new_x, new_y
-x, y = generate()
+
+x, y = generate_food()
+
+def draw_food(x, y):
+    pygame.draw.rect(game_window.screen, Color.RED.value, [x, y, pixel, pixel])
